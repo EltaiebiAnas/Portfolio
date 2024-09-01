de3cad3cf24d3b61d2ddc0fbc39e5a4f76a7e39e
EltaiebiAnas <126774842+EltaiebiAnas@users.noreply.github.com>
2024-09-01 15:05:22 +0100

    tt

--- mail/contact.js
+++ mail/contact.js
@@ -2,19 +2,19 @@ $(function () {
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
-            // Handle the errors
+            // Gérer les erreurs du formulaire
         },
         submitSuccess: function ($form, event) {
-            event.preventDefault(); // Prevent the default form submission
+            event.preventDefault(); // Empêche la soumission par défaut du formulaire
             var name = $("input#name").val();
             var email = $("input#email").val();
             var subject = $("input#subject").val();
             var message = $("textarea#message").val();
 
             $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable the submit button to prevent multiple submissions
+            $this.prop("disabled", true); // Désactiver le bouton pour éviter les soumissions multiples
 
-            // Send email using EmailJS
+            // Envoyer l'email en utilisant EmailJS
             emailjs.send("service_pyods59", "template_v2p9au5", {
                 to_name: "Anass",
                 from_name: name,
@@ -25,15 +25,26 @@ $(function () {
                 `,
             }).then(function(response) {
                 console.log('SUCCESS!', response.status, response.text);
-                alert("Votre message a été envoyé avec succès !"); // Display success alert
-                $this.prop("disabled", false); // Re-enable the submit button
+                $("#alertSuccess").removeClass("d-none").addClass("d-block"); // Afficher l'alerte de succès
+                $this.prop("disabled", false); // Réactiver le bouton
 
-                // Optionally, clear the form fields
+                // Réinitialiser les champs du formulaire
                 $('#contactForm')[0].reset();
+
+                // Cacher l'alerte après 5 secondes
+                setTimeout(function() {
+                    $("#alertSuccess").removeClass("d-block").addClass("d-none");
+                }, 5000);
+
             }, function(error) {
                 console.log('FAILED...', error);
-                alert("Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer plus tard.");
-                $this.prop("disabled", false); // Re-enable the submit button
+                $("#alertError").removeClass("d-none").addClass("d-block"); // Afficher l'alerte d'erreur
+                $this.prop("disabled", false); // Réactiver le bouton
+
+                // Cacher l'alerte après 5 secondes
+                setTimeout(function() {
+                    $("#alertError").removeClass("d-block").addClass("d-none");
+                }, 5000);
             });
         },
         filter: function () {
